c6025e473e8411a2babc357d2ec064413e84c858
Kamil <olejniczak.kam@gmail.com>
2017-09-01 00:27:45 +0200

    projekt kurs kalkulator

---
+++ src/main/java/pl/reaktor/model/Credit.java
@@ -0,0 +1,30 @@
+package pl.reaktor.model;
+
+public class Credit {
+
+	private double kwota_kredyt;
+	private int lr;
+	private double procent;
+	
+	public double getKwota_kredyt() {
+		return kwota_kredyt;
+	}
+	public void setKwota_kredyt(double kwota_kredyt) {
+		this.kwota_kredyt = kwota_kredyt;
+	}
+	public int getLr() {
+		return lr;
+	}
+	public void setLr(int lr) {
+		this.lr = lr;
+	}
+	public double getProcent() {
+		return procent;
+	}
+	public void setProcent(double procent) {
+		this.procent = procent;
+	}
+	
+	
+	
+}
